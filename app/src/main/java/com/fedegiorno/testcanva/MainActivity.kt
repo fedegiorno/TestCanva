6ce85e692ab94ded4eee658eeac4fdff20bae831
fedegiorno <fedegiorno@gmail.com>
2023-02-04 18:53:29 -0300

    Initial commit Aplicacion de prueba. Uso del canva. Funciona pero hay que optimizar

---
+++ app/src/main/java/com/fedegiorno/testcanva/MainActivity.kt
@@ -0,0 +1,195 @@
+package com.fedegiorno.testcanva
+/*
+Creado por Federico Giorno el 31/1/2023
+UTN - FRBA
+*/
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Point
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.sqrt
+
+
+const val MAX_RADIO: Int = 500
+
+class MainActivity : AppCompatActivity(){
+    //private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        //val vista = mainBinding.root
+        val vista = Mivista(this)
+        setContentView(vista)
+        //vista.setOnTouchListener(this)
+    }
+
+    internal class Mivista(context: Context?) : View(context) {
+        private var dX : Float = 0.0f
+        private var dY : Float = 0.0f
+        private var relX : Float = 0.0f
+        private var relY : Float = 0.0f
+        private var cx : Float = 0.0f
+        private var cy : Float = 0.0f
+        //private var radio : Double = 0.0
+        //private var angulo: Double = 0.0
+        private var cuadrante: Int = 0
+        private var amplitud: Double = 0.0
+        private var accion: String = "accion"
+
+        @SuppressLint("DrawAllocation")
+        public override fun onDraw(canvas: Canvas) {
+            val paint = Paint()
+            paint.setColor(Color.BLACK)
+            canvas.drawPaint(paint)
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = 4f
+            paint.color = Color.GREEN
+            val ancho = width
+            val alto = height
+            cx = ancho.div(2f)
+            cy = alto.div(2f)
+            //Log.i("KIRCHOFFF", "cX = " + cx + " cY = " + cy)
+            //circulos concentricos
+            for (radio in 100..MAX_RADIO step 100){
+                canvas.drawCircle(cx, cy, radio.toFloat(), paint)
+            }
+
+            //cuadrantes
+            canvas.drawLine(cx,cy,1040f,cy,paint)
+            canvas.drawLine(cx,cy,40f,cy,paint)
+            canvas.drawLine(cx,cy,cx,532f,paint)
+            canvas.drawLine(cx,cy,cx,1532f,paint)
+
+            //timon
+            paint.style = Paint.Style.FILL
+            paint.color = Color.YELLOW
+
+            when (accion) {
+                "DOWN" -> {
+                    Log.i("KIRCHOFFF", "Cuadrante " + cuadrante)
+                    paint.color = Color.GREEN
+                    if (amplitud >= MAX_RADIO){
+                        when (cuadrante) {
+                            1 -> {
+                                dX = MAX_RADIO*Math.abs(relX).div(amplitud).toFloat() + cx
+                                dY = cy - MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                            }
+                            2 -> {
+                                dX = cx - MAX_RADIO*Math.abs(relX).div(amplitud).toFloat()
+                                dY = cy - MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                                }
+                            3 -> {
+                                dX = cx - MAX_RADIO*Math.abs(relX).div(amplitud).toFloat()
+                                dY = cy + MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                                Log.i("KIRCHOFFF", "relX = " + relX + " relY = " + relY)
+                                Log.i("KIRCHOFFF", "X = " + dX + " Y = " + dY)
+                            }
+                            4 -> {
+                                dX = MAX_RADIO*Math.abs(relX).div(amplitud).toFloat() + cx
+                                dY = MAX_RADIO*Math.abs(relY).div(amplitud).toFloat() + cy
+                            }
+                        }
+                    }
+                    canvas.drawCircle(dX, dY, 100f, paint)
+                    Log.i("KIRCHOFFF", "DOWN")
+                }
+                "MOVE" -> {
+                    paint.color = Color.GREEN
+                    if (amplitud >= MAX_RADIO){
+                        when (cuadrante) {
+                            1 -> {
+                                dX = MAX_RADIO*Math.abs(relX).div(amplitud).toFloat() + cx
+                                dY = cy - MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                            }
+                            2 -> {
+                                dX = cx - MAX_RADIO*Math.abs(relX).div(amplitud).toFloat()
+                                dY = cy - MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                            }
+                            3 -> {
+                                dX = cx - MAX_RADIO*Math.abs(relX).div(amplitud).toFloat()
+                                dY = cy + MAX_RADIO*Math.abs(relY).div(amplitud).toFloat()
+                            }
+                            4 -> {
+                                dX = MAX_RADIO*Math.abs(relX).div(amplitud).toFloat() + cx
+                                dY = MAX_RADIO*Math.abs(relY).div(amplitud).toFloat() + cy
+                            }
+                        }
+                    }
+                    canvas.drawCircle(dX, dY, 100f, paint)
+                    Log.i("KIRCHOFFF", "Amplitud = " + amplitud.toString())
+                    Log.i("KIRCHOFFF", "MOVE")
+                }
+                "UP" -> {
+                    paint.color = Color.YELLOW
+                    canvas.drawCircle(cx, cy, 100f, paint)
+                    Log.i("KIRCHOFFF", "UP")
+                } else  -> {
+                    canvas.drawCircle(cx, cy, 100f, paint)
+                    Log.i("KIRCHOFFF", "ELSE")
+                }
+            }
+        }
+
+        private fun calcularAbsY(relY: Float): Float {
+            dY = relY + cy
+            return dY
+        }
+
+        private fun calcularAbsX(relX: Float): Float {
+            dX = relX + cx
+            return dX
+        }
+
+        override fun onTouchEvent(e: MotionEvent): Boolean {
+            dX = e.x
+            dY = e.y
+            relX = dX - cx
+            relY = cy - dY
+            amplitud = Math.sqrt(Math.pow(dY.toDouble()-cy.toDouble(),2.0)+Math.pow(dX.toDouble()-cx.toDouble(),2.0))
+
+            Log.i("KIRCHOFFF", "Punto absoluto X = " + dX + " Y = " + dY)
+            Log.i("KIRCHOFFF", amplitud.toString())
+
+            if ((relX >= 0) && (relY >= 0)) {cuadrante = 1
+            } else {
+                if ((relX <= 0) && (relY >= 0)) {cuadrante = 2
+                } else {
+                    if ((relX <= 0) && (relY <= 0)) {cuadrante = 3
+                    } else {
+                        if ((relX >= 0) && (relY <= 0)) {cuadrante = 4}
+                    }
+                }
+            }
+            Log.i("KIRCHOFFF", "Cuadrante " + cuadrante.toString())
+
+            //radio = Math.sqrt(Math.pow(dX.toDouble(),dY.toDouble()))
+
+            when(e.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    accion = "DOWN"
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    accion = "MOVE"
+                }
+                MotionEvent.ACTION_UP -> {
+                    accion = "UP"
+                }
+                else -> {
+                    return false
+                }
+            }
+            invalidate()
+            return true
+        }
+    }
+
+}
